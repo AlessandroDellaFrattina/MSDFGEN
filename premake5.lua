264c189d0b3765ffbd5fa7bf85eadba910afeaa4
asmdenc1 <alessandrodellafrattina@gmail.com>
2024-01-07 16:00:14 +0100

    Changed staticruntime from "On" to\ Off"

--- premake5.lua
+++ premake5.lua
@@ -109,7 +109,7 @@ project "MSDFGEN"
 	kind "StaticLib"
 	language "C++"
 	cppdialect "C++17"
-	staticruntime "On"
+	staticruntime "Off"
 	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
