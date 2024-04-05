61af86b47cca6c9e8c8db413aed55b2c4ba46395
asmdenc1 <alessandrodellafrattina@gmail.com>
2024-04-05 18:55:01 +0200

    Updated premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -110,8 +110,8 @@ project "MSDFGEN"
 	language "C++"
 	cppdialect "C++17"
 	staticruntime "Off"
-	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/lib")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/lib")
 
 	includedirs {
 
